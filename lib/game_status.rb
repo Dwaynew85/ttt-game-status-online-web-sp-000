4e9500891f3eaaf0bcf5d5c8791bab36410504ea
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-03-22 05:18:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -59,3 +59,17 @@ def over?(board)
     false
   end
 end
+
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
